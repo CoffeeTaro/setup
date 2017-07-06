0fc59568ca8858df76a78c02ec0315d759350768
Asamin <CoffeeTaro@users.noreply.github.com>
2017-07-06 15:33:14 +0900

    fixed pip setup

---
+++ mac/conda-setup.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+conda update -y conda
+conda update -y --all
+conda install -y cython
+conda install -y seaborn
+conda install -y pyproj
+conda install -y basemap
+conda install -y mpld3
+conda install -y line_profiler
+conda install -y memory_profiler
+conda install -y pillow
+conda install -y tables
+conda install -y numba
+conda install -y mpich2
+conda install -y mpi4py
+conda install -y pymc
+conda install -y -c rpy2
+conda install -y scikit-learn
+conda install -y scilit-image
+conda install -y networkx
+conda install -y gdal
+conda install -y fiona
+pip install mecab-python3
+pip install mpld3
+pip install timeout-decorator
--- mac/pip3_setup.bash
+++ mac/pip3_setup.bash
@@ -1,69 +1,42 @@
-#!/bin/bash
-pip install --upgrade pip
-
-# basic
+#/bin/bash
 pip install numpy
 pip install scipy
 pip install matplotlib
 pip install pandas
-pip install sympy
-
-# for Web
+pip install mecab-python3
 pip install requests
 pip install requests_oauthlib
-
-# mecab bindings
-pip install mecab-python3
-
-# twitter
 pip install tweepy
 pip install twitter
-
-# virtualenv
 pip install virtualenv
 pip install virtualenvwrapper
-
-# Excel
+pip install jupyter 
 pip install xlrd 
 pip install xlwt
-
-# Jupyter
-pip install jupyter 
 pip install jupyterlab
 pip install serverextension enable --py jupyterlab
 pip install bash_kernel
 pip install jupyter_contrib_nbextensions
 jupyter contrib nbextensions install --user
 pip install ipykernel
-pip install ipythonblocks
-
-# machine learning
 pip install scikit-learn
-pip install scikit-image
-pip install networkx
-pip install tensorflow
-#pip install tensorflow-gpu
-
-# graphic
 pip install seaborn
 pip install prettyplotlib
 pip install bokeh
 pip install mpld3
 pip install vispy
-
-# calc distance
 pip install pyproj
-
-# others
-pip install timeout-decorator
 pip install smopy
+pip install requests_oauthlib
 pip install enum34
+pip install networkx
 pip install shapely
 pip install descartes
+pip install sympy
+pip install ipythonblocks
 pip install tables
-
-# acceleration
 pip install cython
 pip install pyopencl
-pip install sphinx
-pip install ansible
+pip install scikit-image
+pip install tensorflow
+#pip install tensorflow-gpu
