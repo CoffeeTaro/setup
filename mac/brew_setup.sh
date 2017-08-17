675c73fb18fb5ffb55ec4cf1dfca7f565dc0adbc
Asamin <CoffeeTaro@users.noreply.github.com>
2017-08-17 13:56:08 +0900

    brew

--- mac/brew_setup.sh
+++ mac/brew_setup.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 brew install coreutils
 brew install git
 brew install pyenv
@@ -7,3 +7,7 @@ brew install rename
 brew install nmap
 brew install mecab
 brew install mecab-ipadic
+brew install goenv
+brew install nodeenv
+brew install rename
+brew install nkf
