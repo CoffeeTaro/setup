32fb635f4ee845247a1345672ea204f660f45200
Asamin <CoffeeTaro@users.noreply.github.com>
2017-08-22 01:33:42 +0900

    fix centos7

--- centos7/pyenv-install.bash
+++ centos7/pyenv-install.bash
@@ -5,11 +5,11 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile 
 pyenv install anaconda3-4.4.0
-pyenv install 3.6.1
+pyenv install 3.6.2
 
 pyenv global anaconda3-4.4.0
 ./conda-setup.bash
 ./pip_setup.bash
 
-pyenv global 3.6.1
+pyenv global 3.6.2
 ./pip_setup.bash
